62fc0176c552789596ccd0bffabee6e28d378bc0
1legennd <miguel772289@gmail.com>
2026-02-14 17:13:04 -0300

    Update index.js
    ok

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+const TOKEN = process.env.TOKEN;
+
+const cartas = [
+  {
+    nome: "Neymar Jr",
+    time: "Al-Hilal",
+    posicao: "PE",
+    liga: "Saudi League",
+    valor: "€60M",
+    habilidade: "Drible mágico",
+    imagem: "https://i.imgur.com/4AiXzf8.jpeg"
+  }
+];
+
+client.once('ready', () => {
+  console.log(`Bot online: ${
